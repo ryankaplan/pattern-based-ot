b104657eda5d33f28fae9f0626e243866c79f783
Ryan Kaplan <ryankap@gmail.com>
2015-10-27 22:15:16 -0700

    Add initial stuffs

---
+++ src/server/server.ts
@@ -0,0 +1,34 @@
+/// <reference path='../typings/node/node.d.ts' />
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Serve data out of /static
+app.use(express.static(__dirname + '/../../'));
+
+var nextSiteId = 1;
+
+io.on('connection', function (socket) {
+    // Give the client a client id
+    socket.emit('site_id', nextSiteId);
+    nextSiteId += 1;
+
+    // msgData is a json-serialized list of operations
+    socket.on("operations", function (msgData) {
+        io.emit("operations", msgData);
+    })
+});
+
+app.get('/', function (req, res) {
+    res.writeHead(302, {
+        'Location': '/html/wootdocument.html'
+    });
+    res.end();
+});
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
